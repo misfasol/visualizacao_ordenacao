81b8e2600e87746265bd7e7b26989788c403d872
Gabriel Schlagenhaufer <gabriel.schlagenhaufer@gmail.com>
2024-10-31 02:19:18 -0300

    barras certo e clicar r

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand/v2"
 	"time"
 
@@ -18,13 +19,16 @@ func CriarLista(n int) []int32 {
 	for i := range lista {
 		lista[i] = int32(i) + 1
 	}
+	return lista
+}
+
+func RandomizarLista(lista []int32) {
 	for i := range lista {
 		temp := lista[i]
-		rand := rand.IntN(n)
+		rand := rand.IntN(len(lista))
 		lista[i] = lista[rand]
 		lista[rand] = temp
 	}
-	return lista
 }
 
 func Swap(a, b *int32) {
@@ -44,22 +48,22 @@ func Sorted(l []int32) bool {
 	return true
 }
 
-func BubbleSort(l []int32) {
+func BubbleSort(l []int32, tempo time.Duration) {
 	for i := range len(l) {
 		for j := range len(l) - i - 1 {
 			if l[j] > l[j+1] {
 				Swap(&l[j], &l[j+1])
 			}
-			time.Sleep(time.Millisecond)
+			time.Sleep(tempo)
 		}
 	}
 }
 
-func GnomeSort(l []int32) {
+func GnomeSort(l []int32, tempo time.Duration) {
 	pos := 0
 	tamanho := len(l)
 	for pos < tamanho {
-		time.Sleep(time.Millisecond)
+		time.Sleep(tempo)
 		if pos == 0 || l[pos] >= l[pos-1] {
 			pos++
 		} else {
@@ -69,16 +73,16 @@ func GnomeSort(l []int32) {
 	}
 }
 
-func MergeSort(l []int32) {
+func MergeSort(l []int32, tempo time.Duration) {
 	if len(l) <= 1 {
 		return
 	}
-	MergeSort(l[:len(l)/2])
-	MergeSort(l[len(l)/2:])
+	MergeSort(l[:len(l)/2], tempo)
+	MergeSort(l[len(l)/2:], tempo)
 	aux := make([]int32, len(l))
 	ie, id, i := 0, len(l)/2, 0
 	for ie < len(l)/2 || id < len(l) {
-		time.Sleep(time.Millisecond * 10)
+		time.Sleep(tempo)
 		if ie < len(l)/2 && (id >= len(l) || l[ie] <= l[id]) {
 			aux[i] = l[ie]
 			ie++
@@ -90,33 +94,53 @@ func MergeSort(l []int32) {
 	}
 	for i := range l {
 		l[i] = aux[i]
-		time.Sleep(time.Millisecond * 10)
+		time.Sleep(tempo)
 	}
 }
 
 func main() {
 
 	lista := CriarLista(100)
+	RandomizarLista(lista)
 
 	fmt.Println(lista)
 
-	go func() {
-		time.Sleep(time.Second)
-		fmt.Println("comecou sort")
-		MergeSort(lista)
-		fmt.Println("terminou sort")
-	}()
+	// go func() {
+	// 	time.Sleep(time.Second)
+	// 	fmt.Println("comecou sort")
+	// 	MergeSort(lista, time.Nanosecond)
+	// 	fmt.Println("terminou sort")
+	// }()
 
 	rl.SetTargetFPS(60)
 	rl.InitWindow(TAMANHO_LARGURA, TAMANHO_ALTURA, "asd")
+	var posx, posy, wid, hei int32 = 0, 0, 0, 0
+	contador := 0
 	for !rl.WindowShouldClose() {
+
+		if rl.IsKeyPressed(rl.KeyR) {
+			RandomizarLista(lista)
+			go func() {
+				time.Sleep(time.Millisecond * 100)
+				fmt.Println("comecou sort")
+				MergeSort(lista, time.Nanosecond*10000000)
+				fmt.Println("terminou sort")
+			}()
+		}
+
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.Black)
 
 		for k, v := range lista {
-			rl.DrawRectangle(int32(k*(TAMANHO_LARGURA/len(lista))), TAMANHO_ALTURA-(v*int32((TAMANHO_ALTURA/len(lista)))), int32(TAMANHO_LARGURA/len(lista)), v*(int32(TAMANHO_ALTURA/len(lista))), rl.White)
+			posx = int32(float32(k) * (float32(TAMANHO_LARGURA) / float32(len(lista))))
+			posy = int32(float32(TAMANHO_ALTURA) - (float32(v) * (TAMANHO_ALTURA / float32(len(lista)))))
+			wid = int32(math.Ceil(float64(TAMANHO_LARGURA) / float64(len(lista))))
+			hei = int32(float32(v) * (float32(TAMANHO_ALTURA) / float32(len(lista))))
+			contador++
+			rl.DrawRectangle(posx, posy, wid, hei, rl.White)
 		}
+		contador = 0
 
 		rl.DrawFPS(10, 10)
 		rl.EndDrawing()
